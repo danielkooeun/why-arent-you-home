6eee419fa9a878e9c2eced4c0e136da928180045
Daniel Koo <fhisa2@gmail.com>
2018-07-28 18:04:48 -0400

    Map + User location implemented

--- App.js
+++ App.js
@@ -1,11 +1,126 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Platform, Text, View } from 'react-native';
+import MapView, { Circle, AnimatedRegion, Animated } from 'react-native-maps';
+import { Constants, Location, Permissions } from 'expo';
 
-export default class App extends React.Component {
+const latitudeDelta = 0.004;
+const longitudeDelta = 0.005;
+
+let fadeLevel = 1;
+let fadeChange = false;
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('Constructing...');
+
+    this.state = {
+      radius: 200,
+      region: null,
+      gameRegionSelected: false,
+      gameRegion: null,
+      userPosition: {
+        latitude: 0,
+        longitude: 0,
+      },
+      updater: true,
+    };
+  }
+
+  componentWillMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      this._getLocationAsync();
+    }
+  }
+
+  componentDidMount() {
+  }
+
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+
+  onGameRegionChange(gameRegion) {
+    this.setState({ gameRegion });
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    const region = {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta,
+      longitudeDelta,
+    };
+    this.onRegionChange(region);
+    this.onGameRegionChange(region);
+    this.setState({
+      gameRegionSelected: true,
+    });
+    console.log(location);
+
+    this.tracker = setInterval(this._getUserLocation, 1000);
+    this.fader = setInterval(this._userFade, 200);
+  }
+
+  _getUserLocation = async () => {
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ userPosition: location.coords });
+  }
+
+  _userFade = async () => {
+    if (fadeChange) {
+      fadeLevel += 0.1;
+      if (fadeLevel >= 1) fadeChange = false;
+    } else {
+      fadeLevel -= 0.1;
+      if (fadeLevel <= 0) fadeChange = true;
+    }
+    console.log(`this is fadeChange: ${fadeChange} and fadeLevel: ${fadeLevel}`);
+    this.setState({ updater: true });
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        { this.state.region &&
+          <MapView
+            initialRegion={ this.state.region }
+            onRegionChange={ (region) => this.onRegionChange(region) }
+            style={ styles.map }>
+            {/* TODO: make relative to the latlon delta */}
+            { this.state.gameRegion &&
+              <View>
+                <Circle
+                  center={ this.state.gameRegion }
+                  radius={ this.state.radius } 
+                  fillColor="rgba(135, 206, 250, 0.2)"
+                  strokeColor="#4682b4"
+                />
+                <Circle
+                  center={ this.state.userPosition }
+                  radius={ this.state.radius * 0.02 } 
+                  fillColor={ `rgba(0, 0, 0, ${ fadeLevel })` }
+                  strokeColor="rgba(0, 0, 0, 0)"
+                />
+              </View>
+            }
+          </MapView>
+        }
+        <View style={ styles.container}>
+          <Text>Hello World!</Text>
+        </View>
       </View>
     );
   }
@@ -13,9 +128,13 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    height: 400,
   },
 });
