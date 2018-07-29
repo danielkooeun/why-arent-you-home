e80ffa32fcf263a09a36925a51cc69caef39bd38
yuyang-wen <yuyang.wen@mail.utoronto.ca>
2018-07-29 00:00:41 -0400

    syntax error

--- app/components/start.component.js
+++ app/components/start.component.js
@@ -24,11 +24,7 @@ export default class Start extends Component {
         <Button
           color='red'
           title='Ready'
-<<<<<<< HEAD
-          onPress={() => this.props.navigation.navigate('hide')}
-=======
-          onPress={ () => this.props.navigation.navigate('seeker') }
->>>>>>> c97c8c29cff89e6d613989b7fcd69a0b4b7561fc
+          onPress={ () => this.props.navigation.navigate('hide') }
         />
       </View>
     );
